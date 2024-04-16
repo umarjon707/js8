d2d18bd7362839b2b35cee1d3f04bc01ca14ece1
Umarjon <urr777777@gmail.com>
2024-04-16 16:47:02 +0500

    cars

--- hw4/index.js
+++ hw4/index.js
@@ -1,63 +1,136 @@
-// let arr = [1, 2, 3, 4, [], true, false,'alex',null,true,[],'ma','xfhfdsgh',false,null] 
-// let num = 0
-// let str = 0
-// let object = 0
-// let others = 0
+let cars = [
+    {
+        mode: "malibu 2",
+        mark: "GM",
+        color: "white",
+        price: 30000,
+        engine: 2.4,
+        year: 2023
+    },
+    {
+        mode: "Tahoe",
+        mark: "GM",
+        color: "black",
+        price: 90000,
+        engine: 3.5,
+        year: 2024
+    },
+    {
+        mode: "Bugatti",
+        mark: "BG",
+        color: "blue",
+        price: 2000000,
+        engine: 5.5,
+        year: 2020
+    },
+    {
+        mode: "Matiz",
+        mark: "Daewoo",
+        color: "green",
+        price: 3000,
+        engine: 5.5,
+        year: 2015
+    },
+    {
+        mode: "Nexia 2",
+        mark: "GM",
+        color: "gray",
+        price: 7000,
+        engine: .6,
+        year: 2010
+    },
+    {
+        mode: "Camry",
+        mark: "Toyota",
+        color: "white",
+        price: 70000,
+        engine: 3.5,
+        year: 2022
+    },
+]
 
 
-// arr.filter((item)=>{
-// if(typeof(item)==='string') {
-// str += item.length;
+ let from = prompt()
+ let up = prompt()
+
+ cars.forEach((cars)=>{
+    if(cars.price>=from && cars.price<=up){
+        alert(cars.mode,cars.price);
+    } 
+}
+)
+ 
+ let expensive = cars.reduce((current,prev) => prev.price > current.price ? prev : current);;
+ console.log(expensive);
+ 
+ 
+ let cheap = cars.reduce((prev, current) => prev.price < current.price ? prev : current);
+ console.log(cheap);
+ 
+ 
+ let fast = cars.reduce((prev, current) => prev.engine > current.engine ? prev : current);
+ console.log(fast);
+ 
+ 
+ let old = cars.reduce((current,prev) => current.year < prev.year ? prev : current);
+ console.log(old);
+
+
+
+
+
+// найти самую дорогую машину 
+// найти самую дешевую машину 
+// найти самую бысрую машину
+// найти самую старую
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Нахождение самой дорогой машины
+// let mostExpensiveCar = cars[0];
+// for (let i = 1; i < cars.length; i++) {
+//     if (cars[i].price > mostExpensiveCar.price) {
+//         mostExpensiveCar = cars[i];
+//     }
 // }
-// })
+// console.log("Самая дорогая машина:", mostExpensiveCar);
 
-// arr.filter((item)=>{
-// if(typeof(item)==='number') {
-//    num++
+// // Нахождение самой дешевой машины
+// let cheapestCar = cars[0];
+// for (let i = 1; i < cars.length; i++) {
+//     if (cars[i].price < cheapestCar.price) {
+//         cheapestCar = cars[i];
+//     }
 // }
-// })
+// console.log("Самая дешевая машина:", cheapestCar);
 
-
-// arr.filter((item)=>{
-// if(typeof(item)==='object') {
-//   object++
+// // Нахождение самой быстрой машины
+// let fastestCar = cars[0];
+// for (let i = 1; i < cars.length; i++) {
+//     if (cars[i].engine > fastestCar.engine) {
+//         fastestCar = cars[i];
+//     }
 // }
-// })
+// console.log("Самая быстрая машина:", fastestCar);
 
-
-// arr.filter((item)=>{
-// if(typeof(item)==='boolean') {
-//  others++
+// // Нахождение самой старой машины
+// let oldestCar = cars[0];
+// for (let i = 1; i < cars.length; i++) {
+//     if (cars[i].year < oldestCar.year) {
+//         oldestCar = cars[i];
+//     }
 // }
-// })
-
-// console.log((str));
-// console.log((num));
-// console.log((object));
-// console.log((others));
-    
-let arr = [1, 2, 3, 4, [], true, false,'alex',null,true,[],'ma','xfhfdsgh',false,null] ;
-
-let num = 0;
-let str = 0;
-let object = 0;
-let others = 0;
-
-arr.forEach(element => {
-    if (Array.isArray(element)) {
-        others += element.length;
-    } else if (typeof element === 'number') {
-        num++;
-    } else if (typeof element === 'string') {
-        str++;
-    } else if (typeof element === 'object' && element === 'boolean') {
-        object++;
-    } else {
-        others++;
-    }
-});
-
-console.log(`Number of numbers: ${num}`);
-console.log(`Number of strings: ${str}`);
-console.log(`Number of objects: ${object}`);
-console.log(`Number of other elements: ${others}`);
+// console.log("Самая старая машина:", oldestCar);
